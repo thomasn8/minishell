949a1bbda63633d475ccbf41c06cfa9871611b4e
Thomas Nanchen <thomasnanchen@hotmail.com>
2022-02-22 15:29:39 +0100

    ajouté struct pour passer + d'args dans l'exec

--- srcs/execution/execution1.c
+++ srcs/execution/execution1.c
@@ -6,7 +6,7 @@
 /*   By: tnanchen <thomasnanchen@hotmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 17:54:23 by tnanchen          #+#    #+#             */
-/*   Updated: 2022/02/22 15:04:34 by tnanchen         ###   ########.fr       */
+/*   Updated: 2022/02/22 15:26:18 by tnanchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,40 +41,39 @@ static int	wait_child(int *status)
 	return (0);
 }
 
-static void	child_exec(t_cmd *cmd, char **env, int n, int *cmd_pipe, int stdin_fd)
+static void	child_exec(t_cmd *cmd, int *cmd_pipe, t_exec *exec)
 {
 	g_child = fork();
 	if (g_child == 0)
 	{
 		if (cmd->first_in_redir != NULL)
-			input_redirections(cmd, stdin_fd);
-		if (n != LAST || cmd->first_out_redir)
+			input_redirections(cmd, exec->stdin_fd);
+		if (exec->n != LAST || cmd->first_out_redir)
 			dup2(cmd_pipe[1], STDOUT_FILENO);
 		close(cmd_pipe[0]);
 		close(cmd_pipe[1]);
 		if (cmd->name)
-			simple_cmd(cmd, env);
+			simple_cmd(cmd, exec->env);
 		free_cmds(cmd);
 		exit(NO_NAME);
 	}
 }
 
-static void	main_exec(t_cmd *cmd, char **env, int n, int *status, int stdin_fd)
+static void	main_exec(t_cmd *cmd, t_exec *exec)
 {
 	int		cmd_pipe[2];
 
 	pipe(cmd_pipe);
-	print_fildes(cmd_pipe);
 	if (is_builtin(cmd))
 	{
-		if (n != LAST)
+		if (exec->n != LAST)
 			dup2(cmd_pipe[1], STDOUT_FILENO);
 		builtins(cmd, STDOUT_FILENO);
 	}
 	else
 	{
-		child_exec(cmd, env, n, cmd_pipe, stdin_fd);
-		wait_child(status);
+		child_exec(cmd, cmd_pipe, exec);
+		wait_child(exec->status);
 	}
 	dup2(cmd_pipe[0], STDIN_FILENO);
 	close(cmd_pipe[0]);
@@ -87,13 +86,19 @@ void	execution(t_cmd *current, char **env, int n, int *status)
 {
 	int		stdin_fd;
 	int		stdout_fd;
+	t_exec	exec;
 
 	stdin_fd = dup(STDIN_FILENO);
 	stdout_fd = dup(STDOUT_FILENO);
-	while (n--)
+	exec.env = env;
+	exec.n = n;
+	exec.status = status;
+	exec.stdin_fd = stdin_fd;
+	exec.stdout_fd = stdout_fd;
+	while (exec.n--)
 	{
 		dup2(stdout_fd, STDOUT_FILENO);
-		main_exec(current, env, n, status, stdin_fd);
+		main_exec(current, &exec);
 		current = current->next;
 	}
 	dup2(stdin_fd, STDIN_FILENO);
